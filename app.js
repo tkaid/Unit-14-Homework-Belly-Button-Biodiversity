26f6d79ba3811b96b4d7ce699aa9e4ead5414585
tkaid <104912335+tkaid@users.noreply.github.com>
2022-09-27 13:48:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ function optionChanged(selectedID){
      panel.append("h6").text(`${key}: ${value}`);
    });
 
+   function buildCharts(sample) {
+
+    // Use `d3.json` to fetch the sample data for the plots
+    d3.json("samples.json").then((data) => {
+      var samples= data.samples;
+      var resultsarray= samples.filter(sampleobject => 
+          sampleobject.id == sample);
+      var result= resultsarray[0]
+    
+      var ids = result.otu_ids;
+      var labels = result.otu_labels;
+      var values = result.sample_values;
 
  // Bubble Chart 
  const trace1 = {
