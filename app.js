1e20e24bb14912301560d3547316f2805eda5d7f
tkaid <104912335+tkaid@users.noreply.github.com>
2022-09-27 13:59:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,119 +1,101 @@
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata= data.metadata;
-    var resultsarray= metadata.filter(sampleobject => 
-      sampleobject.id == sample);
-    var result= resultsarray[0]
-    var panel = d3.select("#sample-metadata");
-    panel.html("");
-    Object.entries(result).forEach(([key, value]) => {
-      panel.append("h6").text(`${key}: ${value}`);
-    });
+import os
 
-  //buildGauge(result.wfreq)
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
 
 
+#################################################
+# Database Setup
+#################################################
 
-  });
-}
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+db = SQLAlchemy(app)
 
-//function buildGauge(wfreq) {}
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
 
-function buildCharts(sample) {
+# Save references to each table
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
 
-// Use `d3.json` to fetch the sample data for the plots
-d3.json("samples.json").then((data) => {
-  var samples= data.samples;
-  var resultsarray= samples.filter(sampleobject => 
-      sampleobject.id == sample);
-  var result= resultsarray[0]
 
-  var ids = result.otu_ids;
-  var labels = result.otu_labels;
-  var values = result.sample_values;
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
 
-//------------------------------------------------------//
-//------------------------------------------------------//
-          // Build a BUBBLE Chart 
-//------------------------------------------------------//
-//------------------------------------------------------//
 
-  var LayoutBubble = {
-    margin: { t: 0 },
-    xaxis: { title: "OTU ID" },
-    hovermode: "closest",
-    };
+@app.route("/names")
+def names():
+    """Return a list of sample names."""
 
-    var DataBubble = [ 
-    {
-      x: ids,
-      y: values,
-      text: labels,
-      mode: "markers",
-      marker: {
-        color: ids,
-        size: values,
-        }
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns)[2:])
+
+
+@app.route("/metadata/<sample>")
+def sample_metadata(sample):
+    """Return the MetaData for a given sample."""
+    sel = [
+        Samples_Metadata.sample,
+        Samples_Metadata.ETHNICITY,
+        Samples_Metadata.GENDER,
+        Samples_Metadata.AGE,
+        Samples_Metadata.LOCATION,
+        Samples_Metadata.BBTYPE,
+        Samples_Metadata.WFREQ,
+    ]
+
+    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+
+    # Create a dictionary entry for each row of metadata information
+    sample_metadata = {}
+    for result in results:
+        sample_metadata["sample"] = result[0]
+        sample_metadata["ETHNICITY"] = result[1]
+        sample_metadata["GENDER"] = result[2]
+        sample_metadata["AGE"] = result[3]
+        sample_metadata["LOCATION"] = result[4]
+        sample_metadata["BBTYPE"] = result[5]
+        sample_metadata["WFREQ"] = result[6]
+
+    print(sample_metadata)
+    return jsonify(sample_metadata)
+
+
+@app.route("/samples/<sample>")
+def samples(sample):
+    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Filter the data based on the sample number and
+    # only keep rows with values above 1
+    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+    # Format the data to send as json
+    data = {
+        "otu_ids": sample_data.otu_id.values.tolist(),
+        "sample_values": sample_data[sample].values.tolist(),
+        "otu_labels": sample_data.otu_label.tolist(),
     }
-  ];
-
-  Plotly.newPlot("bubble", DataBubble, LayoutBubble);
+    return jsonify(data)
 
 
-//---------------------------------------------------------//
-//---------------------------------------------------------//
-              //  Build a BAR Chart
-//---------------------------------------------------------//  
-//---------------------------------------------------------// 
-  var bar_data =[
-    {
-      y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
-      x:values.slice(0,10).reverse(),
-      text:labels.slice(0,10).reverse(),
-      type:"bar",
-      orientation:"h"
-
-    }
-  ];
-
-  var barLayout = {
-    title: "Top 10 Bacteria Cultures Found",
-    margin: { t: 30, l: 150 }
-  };
-
-  Plotly.newPlot("bar", bar_data, barLayout);
-});
-}
- 
-
-function init() {
-// Grab a reference to the dropdown select element
-var selector = d3.select("#selDataset");
-
-// Use the list of sample names to populate the select options
-d3.json("samples.json").then((data) => {
-  var sampleNames = data.names;
-  sampleNames.forEach((sample) => {
-    selector
-      .append("option")
-      .text(sample)
-      .property("value", sample);
-  });
-
-  // Use the first sample from the list to build the initial plots
-  const firstSample = sampleNames[0];
-  buildCharts(firstSample);
-  buildMetadata(firstSample);
-});
-}
-
-function optionChanged(newSample) {
-// Fetch new data each time a new sample is selected
-buildCharts(newSample);
-buildMetadata(newSample);
-}
-
-
-
-// Initialize the dashboard
-init();
+if __name__ == "__main__":
+    app.run()
